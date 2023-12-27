5b89e0c4957de08132640066d889e48374bf1220
Alfredo Neto <eu.alfredoneto@hotmail.com>
2023-12-27 16:48:17 -0400

    Start

---
+++ TodoApp/Services/ITodoService.cs
@@ -0,0 +1,12 @@
+﻿namespace TodoApp.Services
+{
+    public interface ITodoService
+    {
+        public void Add(TodoItem item);
+        public IEnumerable<TodoItem> GetAll();
+        public void Delete(TodoItem item);
+        public void Complete(TodoItem item);
+        public void UnComplete(TodoItem item);
+
+    }
+}
---
+++ TodoApp/Services/TodoService.cs
@@ -0,0 +1,43 @@
+﻿
+namespace TodoApp.Services
+{
+    public class TodoService : ITodoService
+    {
+        private readonly IList<TodoItem> _todoItems;
+
+        public TodoService()
+        {
+            _todoItems = new List<TodoItem>
+            {
+                new TodoItem("Comprar café"),
+                new TodoItem("Passear com a Mulan")
+            };
+        }
+
+        public void Add(TodoItem item)
+        {
+            _todoItems.Add(item);
+        }
+
+        public IEnumerable<TodoItem> GetAll()
+        {
+            return _todoItems.ToList();
+        }
+
+        public void Delete(TodoItem item)
+        {
+            _todoItems.Remove(item);
+        }
+
+        public void Complete(TodoItem item)
+        {
+            item.Completed = true;
+        }
+
+        public void UnComplete(TodoItem item)
+        {
+            item.Completed = false;
+        }
+
+    }
+}
